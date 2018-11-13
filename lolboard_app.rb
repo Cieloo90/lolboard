1eec1ab6c1b7f573b11fda7ec60ed4c070161758
Cieloo90 <ciesielski.lucas@gmail.com>
2018-11-13 11:58:17 +0100

    lolboard_app refactor

--- lolboard_app.rb
+++ lolboard_app.rb
@@ -12,7 +12,7 @@ br.window.move_to(0, 0)
 link = 'https://boards.eune.leagueoflegends.com/en/'
 topics = []
 
-conn_sq = Sequel.postgres(
+Sequel.postgres(
   'lolboard_db_tests',
   user: 'postgres',
   password: 'password',
@@ -27,32 +27,31 @@ class Topics < Sequel::Model
 end
 
 br.goto(link)
-discussion_table = br.div(class: %w[discussions main]).html
-n_discussion_table = Nokogiri::HTML.parse(discussion_table)
+n_discussion_table = Nokogiri::HTML.parse(br.div(class: %w[discussions main]).html)
 
 ### to do \/ pagination on discussion table site \/ ###
 
-n_discussion_table.css('.discussion-list-item').each_with_index do |row, index|
-  topics[index] = {
+n_discussion_table.css('.discussion-list-item').each do |row|
+  topics.push(
     href: row['data-href'],
     unique_code: row['data-discussion-id'],
     comms: row['data-comments']
-  }
+  )
 end
 
-topics.each_with_index do |single_topic, index|
-  unique_code_checker = Topics[unique_code: single_topic[:unique_code]]
+topics.each_with_index do |topic, index|
+  unique_code_checker = Topics[unique_code: topic[:unique_code]]
 
   if !unique_code_checker && index < 10
-    br.goto("https://boards.eune.leagueoflegends.com/#{single_topic[:href]}?show=flat")
-    add_topic(br, conn_sq, single_topic[:unique_code])
-    puts "record with unique_code - #{single_topic[:unique_code]} added to database"
+    br.goto("https://boards.eune.leagueoflegends.com/#{topic[:href]}?show=flat")
+    add_topic(br, topic[:unique_code])
+    puts "record with unique_code - #{topic[:unique_code]} added to database"
 
   elsif unique_code_checker
-    if Topics[unique_code: single_topic[:unique_code]][:comm_amount] != single_topic[:comms].to_i
+    if unique_code_checker[:comm_amount] != topic[:comms].to_i
       puts 'amount of comments changed, need update'
-      br.goto("https://boards.eune.leagueoflegends.com/#{single_topic[:href]}?show=flat")
-      check_comments(br, conn_sq, single_topic[:unique_code])
+      br.goto("https://boards.eune.leagueoflegends.com/#{topic[:href]}?show=flat")
+      check_comments(br, topic[:unique_code])
     else
       puts 'up to date'
     end
